efe23186c20c509b91025d799cde80311a4b4d54
dorianmaissin <153079497+dorianmaissin@users.noreply.github.com>
2024-01-09 13:48:50 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,43 @@
+let jsonData 
+const key = "79DWOl8fX6_GjcUXEAjil5fuZLFH5hn9Lr7MyTd0Epc"
+async function getPhotos() {
+    const key = "79DWOl8fX6_GjcUXEAjil5fuZLFH5hn9Lr7MyTd0Epc"
+    const count = 20
+    const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${key}&count=${count}`
+    try{
+       const url = await fetch(apiUrl)
+       jsonData =  await url.json()
+       console.log(jsonData)
+    } catch(error){
+        alert("404 Error")
+    }
+}
+
+const applyPhotos = () => {
+    getPhotos().then(() => {
+        const galleryContainer = document.querySelector("#gallery-container");
+        for(let i = 0; i < jsonData.length; i++) {
+            const div = document.createElement("div");
+            div.setAttribute("class", "hover-container");
+
+            const img = document.createElement("img");
+            img.setAttribute("src", jsonData[i].urls.regular);
+            img.setAttribute("alt", jsonData[i].alt_description)
+            div.appendChild(img);
+
+            const a = document.createElement("a");
+            a.setAttribute("href", jsonData[i].links.html);
+            a.setAttribute("target", "_blank");
+            a.appendChild(div);
+            galleryContainer.appendChild(a);
+        };    
+    }) 
+}
+applyPhotos()
+
+window.addEventListener('scroll', function() {
+    if(window.innerHeight + window.scrollY >= document.body.offsetHeight){
+        applyPhotos()
+    }
+})
+
